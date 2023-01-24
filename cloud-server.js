a5cd854250b9d898e485765bfc927ed712306480
yashwanth <gownipalli.yeswanth@agilepoint.com>
2023-01-24 14:36:32 +0530

    commit

--- cloud-server.js
+++ cloud-server.js
@@ -6,8 +6,16 @@ function cloudServer(){
             const response = await fetch(apiURL , {method : 'GET'})
             return response.json()
         },
-        set : async function (){
-          
+        set : async function (url , options){
+          const header = new Headers
+          header.append('content-type','application/json');
+          const response = await fetch(url , {
+           ...options,
+           headers : header,
+          })
+        },
+        post : function(){
+            
         },
         put : function(){
 
