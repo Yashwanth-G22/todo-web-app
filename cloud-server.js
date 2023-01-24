78dc09af60a755f1a87a873acf50abd608cbc08a
yashwanth <gownipalli.yeswanth@agilepoint.com>
2023-01-24 13:06:59 +0530

    cpmmit

--- cloud-server.js
+++ cloud-server.js
@@ -2,12 +2,20 @@ function cloudServer(){
     const apiURL = `https://mk-ap-todo-webapi.azurewebsites.net/api/YeswanthTodoItems`;
 
     return{
-        get: async function (){
+        get : async function (){
             const response = await fetch(apiURL , {method : 'GET'})
             return response.json()
         },
+        set : async function (){
+          
+        },
+        put : function(){
 
+        },
+        delete : function(){
+
+        },
     }
 
 }
-cloudServer().get()
+cloudServer().set()
