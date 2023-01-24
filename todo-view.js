6dc408516054d8c9cc1389534693975152ed1454
yashwanth <gownipalli.yeswanth@agilepoint.com>
2023-01-24 16:57:24 +0530

    commit

--- todo-view.js
+++ todo-view.js
@@ -2,7 +2,11 @@ import  cloudServer  from "./cloud-server.js";
 
 console.log(cloudServer().post('yash'))
 
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const ul = document.querySelector('.taskList');
 
 function todoView(){
 
+    
 }
