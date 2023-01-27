c0ec84f4598d7642dd988e5dfc5b7268d9e6d1e8
yashwanth <gownipalli.yeswanth@agilepoint.com>
2023-01-27 16:53:17 +0530

    commit for localstorage server

--- localStorage-server.js
+++ localStorage-server.js
@@ -4,15 +4,20 @@ function localServer() {
             const todoList = (localStorage.getItem('todos'))? JSON.parse(localStorage.getItem('todos')) : []
             return todoList;
         },
-        set: function set() {
-
+        set: function set(todo) {
+            let set_Todo = this.get()
+            set_Todo.push(todo)
+            localStorage.setItem('todos',JSON.stringify(set_Todo))
         },
-
-        delete: function delet() {
-
+        delete: function delet(index) {
+            let delete_Todo = this.get()
+            delete_Todo.splice(index , 1)
+            localStorage.setItem('todos',JSON.stringify(delete_Todo))
         },
         deleteAll: function deleteAll() {
-
+            let deleteAll_Todo = this.get()
+            deleteAll_Todo = []
+            localStorage.setItem('todos',JSON.stringify(deleteAll_Todo))
         },
     }
 }
