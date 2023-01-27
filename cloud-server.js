33b9e3cf2695de820ce85bef000314fc5b6593e3
yashwanth <gownipalli.yeswanth@agilepoint.com>
2023-01-27 11:55:39 +0530

    commit

--- cloud-server.js
+++ cloud-server.js
@@ -1,53 +1,51 @@
- function cloudServer(){
+function cloudServer() {
     const apiURL = `https://mk-ap-todo-webapi.azurewebsites.net/api/YeswanthTodoItems`;
 
-    return{
-        get : async function (){
+    return {
+        get: async function () {
             console.log('yash')
-            const response = await fetch(apiURL , {method : 'GET'})
+            const response = await fetch(apiURL, { method: 'GET' })
             return response.json()
         },
-        set : async function (url , options){
-          const header = new Headers
-          header.append('content-type','application/json');
-          const response = await fetch(url , {
-           ...options,
-           headers : header,
-          })
-          return response.json()
+        set: async function (url, options) {
+            const header = new Headers
+            header.append('content-type', 'application/json');
+            return fetch(url, {
+                ...options,
+                headers: header,
+            })
         },
-        post : async function(name){
-          const result = await this.set(apiURL,{
-                method : 'POST',
-                body : JSON.stringify({
-                    name : name
+        post: async function (name) {
+            const response = await this.set(apiURL, {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: name
                 })
             })
-            return result;
+            return response.json();
         },
-        put : function(id , name){
-          const result =  this.set(`${apiURL}/${id}`,{
-                method :'PUT',
-                body : JSON.stringify({
-                    "id" : id ,
-                    "name" : name ,
-                    "isCompleted" : true
+        put: async function (id, name) {
+            await this.set(`${apiURL}/${id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    "id": id,
+                    "name": name,
+                    "isCompleted": true
                 })
             })
-            return result
         },
-        delete : function(id){
-            this.set(`${apiURL}/${id}`,{
-                method : 'DELETE',
+        delete: function (id) {
+            this.set(`${apiURL}/${id}`, {
+                method: 'DELETE',
             })
         },
-        deleteAll : function(){
-            this.set(`https://mk-ap-todo-webapi.azurewebsites.net/deleteAll`,{
-                method : 'DELETE'
+        deleteAll: function () {
+            this.set(`https://mk-ap-todo-webapi.azurewebsites.net/deleteAll`, {
+                method: 'DELETE'
             })
         }
     }
-    
+
 }
 export default cloudServer
 // let result = cloudServer().get()
