a328a67b9d8ca14eaec6c2e4f744803ee88f5dc6
yashwanth <gownipalli.yeswanth@agilepoint.com>
2023-02-09 18:54:39 +0530

    commit

--- src/model/localStorage-server.js
+++ src/model/localStorage-server.js
@@ -1,31 +1,36 @@
 function localServer() {
     return {
         get: function get() {
-            const todoList = (localStorage.getItem('todos'))? JSON.parse(localStorage.getItem('todos')) : []
+            const todoList = (localStorage.getItem('todos')) ? JSON.parse(localStorage.getItem('todos')) : []
             return todoList;
         },
         set: function set(todo) {
             let set_Todo = this.get()
             set_Todo.push(todo)
-            localStorage.setItem('todos',JSON.stringify(set_Todo))
+            sendTodo(set_Todo)
             return set_Todo;
         },
-        edit: function edit(index,elem){
+        edit: function edit(index, elem) {
             let edit_Todo = this.get()
-            edit_Todo.splice(index , 1,elem)
-            localStorage.setItem('todos',JSON.stringify(edit_Todo))
+            edit_Todo.splice(index, 1, elem)
+            sendTodo(edit_Todo)
         },
 
         delete: function delet(index) {
             let delete_Todo = this.get()
-            delete_Todo.splice(index , 1)
-            localStorage.setItem('todos',JSON.stringify(delete_Todo))
+            delete_Todo.splice(index, 1)
+            sendTodo(delete_Todo)
         },
         deleteAll: function deleteAll() {
             let deleteAll_Todo = this.get()
             deleteAll_Todo = []
-            localStorage.setItem('todos',JSON.stringify(deleteAll_Todo))
+            sendTodo(deleteAll_Todo)
         },
     }
 }
+        function sendTodo(todo) {
+            localStorage.setItem('todos', JSON.stringify(todo))
+        }
+        
 export default localServer
+
